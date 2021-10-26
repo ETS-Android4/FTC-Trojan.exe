8e5a182f46f29cc5c8f89509b3edece2f3d0c341
MarianGabi <marianneamtu29@yahoo.com>
2021-10-26 21:40:35 +0300

    am trecut si eu pe aici hepa dnb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-public class TeleOp1 extends OpMode {
+public class TeleOp1 extends OpMode {      /// eu am fost aici:))))hepa dnb
 
     @Override
     public void init() {
