001fec288c0cded8c9f3f2d40e677c7b491f07e2
Rusu_David <rusdaid1234@gmail.com>
2021-10-29 16:18:28 +0300

    Am declarat un motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;  //IMportam fisierul pt clasa OpMode
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class TeleOp extends OpMode {
+
+    DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+
     @Override
     public void init() {
 
