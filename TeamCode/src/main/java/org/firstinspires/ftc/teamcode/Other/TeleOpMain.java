b6bb6e80c36345a726de925741b8e87557decd58
DVD-R-2 <91427703+DVD-R-2@users.noreply.github.com>
2021-10-31 21:35:57 +0200

    Niste organizare. (Pachete pe Autonome, TeleOpuri si teste)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeleOpMain.java
@@ -1,8 +1,9 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Other;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
@@ -12,25 +13,29 @@ public class TeleOpMain extends OpMode
 {
     DcMotor roataStanga, roataDreapta;
     DcMotor brat_1, brat_2;
-    Servo servo_1, servo_2;
+    Servo  servo_1, servo_2;
 
     @Override
     public void init()
     {
-
+        //Motoare
         roataStanga  = hardwareMap.get(DcMotor.class, "motorStanga");
         roataDreapta = hardwareMap.get(DcMotor.class, "motorDreapta");
         brat_1       = hardwareMap.get(DcMotor.class, "motor1");
         brat_2       = hardwareMap.get(DcMotor.class, "motor2");
 
+        //Servouri
         servo_1 = hardwareMap.get(Servo.class, "servo1");
         servo_2 = hardwareMap.get(Servo.class, "servo2");
+
+        roataDreapta.setDirection(DcMotorSimple.Direction.REVERSE);    //Directie motoare
+        roataStanga.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void loop()
     {
-        roataStanga.setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
+        roataStanga.setPower(Range.clip(gamepad1.left_stick_y,   -1, 1));
         roataDreapta.setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
 
 //        if(Range.clip(gamepad1.left_stick_x, -1, 1) <= 0)
