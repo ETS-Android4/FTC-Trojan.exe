881d4be20c1fa0aeb01a4815f6c0e0bc0cf74b69
noraradu <93474845+noraradu@users.noreply.github.com>
2021-11-01 12:39:09 +0200

    am scris un comentariu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeleOp1.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Teleop1", group = "Teste")
 
 public class TeleOp1 extends OpMode     /// eu am fost aici:))))hepa dnb
-{
+{                                      ///salut
 
     @Override
     public void init()
