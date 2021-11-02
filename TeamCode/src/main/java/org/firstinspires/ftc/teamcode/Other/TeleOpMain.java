813e4a361217744f7b5e80d12f541458faca85cb
Rusu_David <rusdaid1234@gmail.com>
2021-11-02 17:44:21 +0200

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/TeleOpMain.java
@@ -27,8 +27,8 @@ public class TeleOpMain extends OpMode
         servo_1 = hardwareMap.get(Servo.class, "servo1");
         servo_2 = hardwareMap.get(Servo.class, "servo2");
 
-        roataDreapta.setDirection(DcMotorSimple.Direction.REVERSE);    //Directie motoare
-        roataStanga.setDirection(DcMotorSimple.Direction.FORWARD);
+        roataDreapta.setDirection(DcMotorSimple.Direction.FORWARD);    //Directie motoare
+        roataStanga.setDirection(DcMotorSimple.Direction.REVERSE);
         brat_1.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
@@ -39,7 +39,13 @@ public class TeleOpMain extends OpMode
         roataStanga.setPower(Range.clip(gamepad1.left_stick_y, -1, 1));   //Gamepad 1
         roataDreapta.setPower(Range.clip(gamepad1.right_stick_y, -1, 1));
 
-        brat_1.setPower(Range.clip(gamepad2.left_stick_y, -.5, .5));        //Gamepad 2
-
+        if(gamepad2.b)                                                              //Gamepad 2
+        {
+            brat_1.setPower(Range.clip(gamepad2.left_stick_y, -.7, .7));
+        }
+        else
+        {
+            brat_1.setPower(Range.clip(gamepad2.left_stick_y, -1, 1));
+        }
     }
 }
