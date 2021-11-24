fb581cdac91e3f4d370b92efedd30d0078b8895e
Rusu_David <rusdaid1234@gmail.com>
2021-11-24 14:50:33 +0200

    Am adaugat un nou sistem de viraje.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/Viraje.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Other/Viraje.java
@@ -18,8 +18,8 @@ public class Viraje extends OpMode {
         roataStanga  = hardwareMap.get(DcMotor.class, "motorStanga");
         roataDreapta = hardwareMap.get(DcMotor.class, "motorDreapta");
 
-        roataDreapta.setDirection(DcMotorSimple.Direction.REVERSE);    //Directie motoare
-        roataStanga.setDirection(DcMotorSimple.Direction.FORWARD);
+        roataDreapta.setDirection(DcMotorSimple.Direction.FORWARD);    //Directie motoare
+        roataStanga.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
@@ -29,12 +29,21 @@ public class Viraje extends OpMode {
 
         double left;
         double right;
+        double max;
 
         double fata_spate = gamepad1.left_stick_y;
         double stanga_dreapta = gamepad1.right_stick_x;        //Range.clip?
 
         left = fata_spate - stanga_dreapta;
         right = fata_spate + stanga_dreapta;
+//
+//        max = Math.max(Math.abs(left), Math.abs(right));
+//        if (max > 1.0)
+//        {
+//            left /= max;
+//            right /= max;
+//        }
+
 
         roataStanga.setPower(left);
         roataDreapta.setPower(right);
