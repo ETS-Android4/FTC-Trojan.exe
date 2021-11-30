e8ecd5d21ab51e09a49615f09916518aca69c10d
Karina2929 <horvatkarina7@gmail.com>
2021-11-30 13:58:45 +0200

    am terminat.avem o eroare.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grupa1/autonoma_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grupa1/autonoma_1.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Grupa1;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 public class autonoma_1 extends LinearOpMode
@@ -18,6 +19,12 @@ public class autonoma_1 extends LinearOpMode
     while (opModeIsActive()) { }
 
     }
+    public void stopMotor()
+    {
+        fer.roataStanga.setPower(0);
+        fer.roataDreapta.setPower(0);
+    }
+
     public void fata_spate (double putere, int distanta)  ///pentru deplasarea in spate se pune parametrul putere cu -
     {
         fer.roataStanga.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -39,11 +46,6 @@ public class autonoma_1 extends LinearOpMode
 
     }
 
-   public void stopMotor()
-    {
-        fer.roataStanga.setPower(0);
-        fer.roataDreapta.setPower(0);
-    }
     public void dreapta (double putere, int distanta)
     {
         fer.roataStanga.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -56,12 +58,61 @@ public class autonoma_1 extends LinearOpMode
         fer.roataDreapta.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         fer.roataStanga.setPower(putere);
-        fer.roataDreapta.setPower(putere);
+        fer.roataDreapta.setPower(0);
 
-        while(fer.roataStanga.isBusy() && fer.roataDreapta.isBusy() )
+        while(fer.roataStanga.isBusy() )
         {}
 
         stopMotor();
+    }
+
+    public void stanga (double putere, int distanta)
+    {
+        fer.roataStanga.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fer.roataDreapta.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fer.roataStanga.setTargetPosition(distanta);
+        fer.roataDreapta.setTargetPosition(distanta);
+
+        fer.roataStanga.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fer.roataDreapta.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fer.roataStanga.setPower(0);
+        fer.roataDreapta.setPower(putere);
+
+        while(fer.roataDreapta.isBusy() )
+        {}
+
+        stopMotor();
+    }
+
+    public void peria (double putere, int rotatii)
+    {
+        fer.peria.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fer.peria.setDirection(DcMotorSimple.Direction.FORWARD);
+        fer.peria.setTargetPosition(rotatii);
+        fer.peria.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fer.peria.setPower(putere); ///pentru directia fata se pune cu + si pt spate cu -
+
+        while(fer.peria.isBusy() )
+        {}
+
+        stopMotor();
+    }
+
+    public void brat (double putere, int rotatii)
+    {
+        fer.brat_1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fer.brat_1.setDirection(DcMotorSimple.Direction.FORWARD); ///nu stim cum e pus exact motorul, ne uitam la scoala pt directie
+        fer.brat_1.setTargetPosition(rotatii);
+        fer.brat_1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fer.brat_1.setPower(putere);
+
+        while(fer.brat_1.isBusy() )
+        {}
+
+        stopMotor();
+
     }
 }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grupa1/autonoma_2.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Grupa1;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name= "autonoma_2", group= "Grupa1")
+public class autonoma_2 extends autonoma_1
+{
+    hwgrupa1 fer = new hwgrupa1();
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        fer.init( hwgrupa1, true);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            fata_spate(1,120);
+            dreapta(1,20);  ///am estimat 20 pt intoarcere la dreapta de 90 de grade
+            fata_spate(1,120);
+            dreapta(1,20);
+            fata_spate(1,120);
+            dreapta(1,20);
+            fata_spate(1,120);
+            brat(0.5,5) ;   ///am estimat roatiile
+            peria(-1,20);   ///peria se misca in spate
+        }
+    }
+}
